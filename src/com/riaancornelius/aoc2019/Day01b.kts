903a08bdc26e911de537efb45239881ae7d1bf75
Riaan Cornelius <RiaanC@***>
2019-12-07 08:24:51 +0200

    This is a job for recursion!

--- src/com/riaancornelius/aoc2019/Day01b.kts
+++ src/com/riaancornelius/aoc2019/Day01b.kts
@@ -1,38 +1,33 @@
 package com.riaancornelius.aoc2019
 
 import com.riaancornelius.aoc2019.input.Input
-import kotlin.math.max
 import kotlin.math.truncate
 
 class Day01b {
 
     fun readFileLineByLineUsingForEachLine() : Int {
         val input = Input()
-        return input.readLines(1)
+        return input.readLines(1, false)
             .mapToInt{ calculateFuelForModule(it.toDouble()) }
-            .map { calculateFuelIncludingFuel(it) }
+            .map { calculateFuelIncludingFuel(it, 0) }
             .sum()
     }
 
     private fun calculateFuelForModule(mass: Double): Int {
-        // mass, divide by three, round down, and subtract 2.
-        val fuelRequired = truncate(mass / 3).toInt() - 2
-//        println("Module weight: $mass requires: $fuelRequired")
-        return fuelRequired
+        return truncate(mass / 3).toInt() - 2
     }
 
-    private fun calculateFuelIncludingFuel(initialFuel: Int): Int {
-        var newFuelRequired :Int = Day01b().calculateFuelForModule(initialFuel.toDouble())
-        var totalFuelRequired :Int = newFuelRequired
-        while (newFuelRequired > 0){
-            newFuelRequired = Day01b().calculateFuelForModule(newFuelRequired.toDouble())
-            totalFuelRequired += max(newFuelRequired, 0)
+    private tailrec fun calculateFuelIncludingFuel(newFuelRequired: Int, totalFuel: Int): Int {
+        println("  recursion: $newFuelRequired with total: $totalFuel")
+        return if (newFuelRequired <= 0) {
+            println("    returning $totalFuel")
+            totalFuel
+        } else {
+            calculateFuelIncludingFuel(Day01b().calculateFuelForModule(newFuelRequired.toDouble()),
+                totalFuel + newFuelRequired)
         }
-        totalFuelRequired += initialFuel
-//        println("    Including fuel: $totalFuelRequired")
-        return totalFuelRequired
     }
 }
 
 val totalFuelRequired = Day01b().readFileLineByLineUsingForEachLine()
-println("Part 2: Total fuel required: $totalFuelRequired")
+println("\nPart 2: Total fuel required: $totalFuelRequired")
