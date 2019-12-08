7e85867206586200cb2a1aaec979accc18af9680
Riaan Cornelius <RiaanC@***>
2019-12-08 08:56:17 +0200

    Day 8 Part 1

---
+++ src/com/riaancornelius/aoc2019/Day08.kts
@@ -0,0 +1,34 @@
+package com.riaancornelius.aoc2019
+
+import com.riaancornelius.aoc2019.input.Input
+import java.lang.RuntimeException
+
+class Day08 {
+    private var input = readInput()
+
+    private fun readInput(): String {
+        return Input().readText(8, false).trim()
+    }
+
+    fun decode(width: Int, height:Int) {
+        val charsPerLayer = width * height
+        val chunked = input.chunked(charsPerLayer)
+            .mapIndexed{ idx, it -> idx to it}.toMap()
+//        chunked.entries.forEach { entry -> println("[${entry.key}] ${entry.value}")}
+
+        val minIndex = chunked
+            .mapValues { it.value.count { char -> char=='0' } }
+            .minBy { it.value }?.key
+
+        println("least 0's: [$minIndex] ${chunked[minIndex]}")
+
+        val grouping = chunked[minIndex]
+            ?.groupingBy { it }
+            ?.eachCount()
+
+        println("result: ${grouping?.getValue('1')?.times(grouping.getValue('2'))}")
+    }
+}
+
+// Part 1:
+Day08().decode(25, 6)
