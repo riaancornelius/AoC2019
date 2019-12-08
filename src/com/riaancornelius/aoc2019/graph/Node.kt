2aa7d9d206b4859be77da46ff4f77f66508a9e21
Riaan Cornelius <RiaanC@***>
2019-12-08 19:15:42 +0200

    Graph implementation for day 6

---
+++ src/com/riaancornelius/aoc2019/graph/Node.kt
@@ -0,0 +1,10 @@
+package com.riaancornelius.aoc2019.graph
+
+data class Node(val name: String) {
+    var segments: MutableList<Segment> = mutableListOf()
+}
+
+data class Segment(val node: Node, val distance: Int) {
+    // properties used in the shortest path alg.
+    var calculatedDistFromSource: Int = 0
+}
