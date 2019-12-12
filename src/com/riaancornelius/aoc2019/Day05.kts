dd81a95a6e4b548f21ca750a22cdf5d09b37d197
Riaan Cornelius <RiaanC@***>
2019-12-12 09:09:03 +0200

    Refactor Computer to cater for day 5 part 2

--- src/com/riaancornelius/aoc2019/Day05.kts
+++ src/com/riaancornelius/aoc2019/Day05.kts
@@ -5,5 +5,15 @@ import com.riaancornelius.aoc2019.intcode.Computer
 
 val inputString = Input().readText(5, false)
 // Part 1:
-println("Part 1: ${Computer(inputString).runCalculation(arrayOf(1).toIntArray())}")
+println("Part 1: ${Computer(inputString, true).runCalculation(arrayOf(1).toIntArray())}")
+println(" ====================================================== ")
+
+// Part 2:
+val exampleInputString = Input().readText(5, true)
+println("Part 2 (test) Input 7: ${Computer(exampleInputString, debug = false).runCalculation(arrayOf(7).toIntArray())}")
+println("Part 2 (test) Input 8: ${Computer(exampleInputString).runCalculation(arrayOf(8).toIntArray())}")
+println("Part 2 (test) Input 9: ${Computer(exampleInputString).runCalculation(arrayOf(9).toIntArray())}")
+println(" ====================================================== ")
+
+println("Part 2: ${Computer(inputString).runCalculation(arrayOf(5).toIntArray())}")
 
--- src/com/riaancornelius/aoc2019/intcode/Computer.kt
+++ src/com/riaancornelius/aoc2019/intcode/Computer.kt
@@ -1,6 +1,6 @@
 package com.riaancornelius.aoc2019.intcode
 
-class Computer(private val inputString :String) {
+class Computer(private val inputString :String, private val debug :Boolean = false) {
 
     private val instructions = readInput()
     
@@ -22,45 +22,84 @@ class Computer(private val inputString :String) {
             val currentInstruction = Instruction.parse(instructions[pointer])
             val currentOpCode = currentInstruction.opCode
 
-            println("Checking position [$pointer] with value $currentInstruction")
-            pointer += when (currentOpCode){
+            if (debug) println("Checking position [$pointer] with value ${instructions[pointer]} {$currentInstruction}")
+            if (debug) print("${instructions[pointer]} ")
+
+            when (currentOpCode){
                 1 -> {
+                    if (debug) print(" -> ${instructions[pointer + 1]}, ${instructions[pointer + 2]}, ${instructions[pointer + 3]}")
                     val param1 = read(pointer + 1, currentInstruction.mode1)
                     val param2 = read(pointer + 2, currentInstruction.mode2)
                     write(pointer + 3, param1 + param2)
-                    println(" -> ${instructions[pointer + 1]}, ${instructions[pointer + 2]}, ${instructions[pointer + 3]}")
-                    4
+                    pointer += 4
                 }
                 2 -> {
+                    if (debug) print(" -> ${instructions[pointer + 1]}, ${instructions[pointer + 2]}, ${instructions[pointer + 3]}")
                     val param1 = read(pointer + 1, currentInstruction.mode1)
                     val param2 = read(pointer + 2, currentInstruction.mode2)
                     write(pointer + 3, param1 * param2)
-                    println(" -> ${instructions[pointer + 1]}, ${instructions[pointer + 2]}, ${instructions[pointer + 3]}")
-                    4
+                    pointer += 4
                 }
                 3 -> {
+                    if (debug) print(" -> ${instructions[pointer + 1]}, input[$inputPointer] = ${input[inputPointer]}")
                     write(pointer + 1, input[inputPointer++])
-                    println(" -> ${instructions[pointer + 1]}}")
-                    2
+                    pointer += 2
                 }
                 4 -> {
-                    println(" -> ${instructions[pointer + 1]}}")
+                    if (debug) print(" -> ${instructions[pointer + 1]}")
                     output.add(read(pointer + 1, currentInstruction.mode1))
-                    2
+                    pointer += 2
+                }
+                5 -> {
+                    if (debug) print(" -> ${instructions[pointer + 1]}, ${instructions[pointer + 2]}")
+                    val condition = read(pointer + 1, currentInstruction.mode1)
+                    if (condition != 0) {
+                        pointer = read(pointer + 2, currentInstruction.mode2)
+                        if (debug) print("Setting pointer to $pointer")
+                    } else
+                        pointer += 3
+                }
+                6 -> {
+                    if (debug) print(" -> ${instructions[pointer + 1]}, ${instructions[pointer + 2]}")
+                    val condition = read(pointer + 1, currentInstruction.mode1)
+                    if (condition == 0) {
+                        pointer = read(pointer + 2, currentInstruction.mode2)
+                        if (debug) print("Setting pointer to $pointer")
+                    } else
+                        pointer += 3
+                }
+                7 -> {
+                    if (debug) print(" -> ${instructions[pointer + 1]}, ${instructions[pointer + 2]}, ${instructions[pointer + 3]}")
+                    val param1 = read(pointer +1, currentInstruction.mode1)
+                    val param2 = read(pointer +2, currentInstruction.mode2)
+                    write(pointer +3, run {if(param1 < param2) 1 else 0})
+                    pointer += 4
+                }
+                8 -> {
+                    if (debug) print(" -> ${instructions[pointer + 1]}, ${instructions[pointer + 2]}, ${instructions[pointer + 3]}")
+                    val param1 = read(pointer +1, currentInstruction.mode1)
+                    val param2 = read(pointer +2, currentInstruction.mode2)
+                    write(pointer + 3, run {if(param1 == param2) 1 else 0})
+                    pointer += 4
                 }
                 99 -> break@programLoop
                 else -> throw RuntimeException("Unsupported OpCode found [$currentOpCode]")
             }
+            if (debug) println("\n")
         }
 
+        println()
         return output
     }
 
-    fun read(position :Int, mode:Mode) :Int {
-        return if (mode == Mode.IMMEDIATE) instructions[position] else instructions[instructions[position]]
+    fun read(position :Int, mode:Mode = Mode.POSITION) :Int {
+        val value = if (mode == Mode.IMMEDIATE) instructions[position] else instructions[instructions[position]]
+        if (debug) print( " << read value $value ")
+        return value
     }
 
     private fun write(position :Int, value :Int){
+        if (debug) print( " >> writing $value to position [${instructions[position]}]")
         instructions[instructions[position]] = value
     }
 
@@ -68,7 +107,7 @@ class Computer(private val inputString :String) {
         companion object {
             fun parse(input: Int) : Instruction {
                 val string = input.toString().padStart(5, '0')
-                println("Parsing $input padded to $string")
+//                println("Parsing $input padded to $string")
                 return Instruction(
                     string.substring(string.lastIndex-1, string.length).toInt(),
                     when(string[2]) {
@@ -88,7 +127,19 @@ class Computer(private val inputString :String) {
         }
 
         override fun toString(): String {
-            return "Instruction(opCode=$opCode, mode1=$mode1, mode2=$mode2, mode3=$mode3)"
+            val description = when (opCode) {
+                1 -> "ADDITION"
+                2 -> "MULTIPLICATION"
+                3 -> "READ_INPUT"
+                4 -> "WRITE_OUTPUT"
+                5 -> "JUMP_IF_TRUE"
+                6 -> "JUMP_IF_FALSE"
+                7 -> "LESS_THAN"
+                8 -> "EQUALS"
+                99 -> "TERMINATE"
+                else -> "UNKNOWN"
+            }
+            return "Instruction($description: opCode=$opCode, mode1=$mode1, mode2=$mode2, mode3=$mode3)"
         }
     }
 
